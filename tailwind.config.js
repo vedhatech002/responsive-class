84abe21fa33b807d1faa64587d118d82888a70d0
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-10-15 01:29:26 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'main-grid': '3fr 1fr'
+      }
+    },
+  },
+  plugins: [],
+}
